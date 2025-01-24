29427a36695dd673a02a14a511a7b38371c909be
MrJerry16 <153770755+MrJerry16@users.noreply.github.com>
2025-01-24 11:03:29 +0530

    hey

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
+import {JobCreateComponent} from './job-create/job-create.component';
+import {JobListComponent} from './job-list/job-list.component';
+import {JobEditComponent} from './job-edit/job-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path : '' , component:HomePageComponent ,title: 'HomePage'},
+  {path : 'jobs/create' , component:JobCreateComponent ,title: 'JobCreate'},
+  {path : 'jobs' , component:JobListComponent ,title: 'Jobs List'},
+  {path : 'jobs/edit/:jobid' , component:JobEditComponent ,title: 'JobEdit'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { JobCreateComponent } from './job-create/job-create.component';
+import {HttpClientModule} from '@angular/common/http';
+import { JobListComponent } from './job-list/job-list.component';
+import { JobEditComponent } from './job-edit/job-edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    JobCreateComponent,
+    JobListComponent,
+    JobEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
