5bfcd233d9c4e8100dddf3cc670e6aa3439f74d1
MrJerry16 <153770755+MrJerry16@users.noreply.github.com>
2025-01-30 10:53:10 +0530

    first commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ import {JobEditComponent} from './job-edit/job-edit.component';
 
 const routes: Routes = [
 
-  {path : '' , component:HomePageComponent ,title: 'HomePage'},
-  {path : 'jobs/create' , component:JobCreateComponent ,title: 'JobCreate'},
-  {path : 'jobs' , component:JobListComponent ,title: 'Jobs List'},
-  {path : 'jobs/edit/:jobid' , component:JobEditComponent ,title: 'JobEdit'},
-
+  { path: '', redirectTo: 'jobs/create', pathMatch: 'full' }, // Redirect default path to JobCreateComponent
+  { path: 'jobs/create', component: JobCreateComponent, title: 'JobCreate' },
+  { path: 'jobs', component: JobListComponent, title: 'Jobs List' },
+  { path: 'jobs/edit/:jobid', component: JobEditComponent, title: 'JobEdit' },
+  { path: '**', redirectTo: 'jobs/create' }
 
 ];
 
